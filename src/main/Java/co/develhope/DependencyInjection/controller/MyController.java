46f2f64106f2b366debec61356fcf1ea30cb515a
Mario Di Cristofano <io@gmail.com>
2022-11-29 22:09:29 +0100

    update

--- src/main/Java/co/develhope/DependencyInjection/controller/MyController.java
+++ src/main/Java/co/develhope/DependencyInjection/controller/MyController.java
@@ -1,6 +1,5 @@
 package co.develhope.DependencyInjection.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import co.develhope.DependencyInjection.service.MyService;
